40118015103fe626942f4ae4037b039211ef37da
Chieper <MenAdinEmin@gmail.com>
2024-02-15 12:56:09 +0300

    feat: prettier logs in log.txt

--- utils/common.ts
+++ utils/common.ts
@@ -20,3 +20,14 @@ export const shuffle = (array: Array<string>) => {
 
     return array
 }
+
+export const convertDateToCustomFormat = (dateString: Date) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    return `${day}.${month} ${hours}:${minutes}:${seconds}`;
+}
--- utils/logger.ts
+++ utils/logger.ts
@@ -1,16 +1,17 @@
-import { Logger } from "tslog"
-import { appendFileSync } from "fs"
+import {Logger} from "tslog"
+import {appendFileSync} from "fs"
+import {convertDateToCustomFormat} from "./common";
 
 export function makeLogger(name: string) {
-    const logger = new Logger({ 
-        hideLogPositionForProduction: true, 
+    const logger = new Logger({
+        hideLogPositionForProduction: true,
         name: name,
         prettyLogTemplate: '{{dd}}.{{mm}} {{hh}}:{{MM}}:{{ss}}\t{{logLevelName}}\t{{name}}\t',
         prettyLogTimeZone: 'local',
     })
-    
+
     logger.attachTransport((logObj) => {
-        appendFileSync("./log.txt", JSON.stringify(logObj) + "\n")
+        appendFileSync("./log.txt", `${convertDateToCustomFormat(logObj._meta.date)} ${logObj._meta.logLevelName}   ${logObj._meta.name}      ${logObj[0]}` + "\n")
     })
 
     return logger
